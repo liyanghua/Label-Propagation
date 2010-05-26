b7b50288b3bf1a1135cda26fe4f3888be8371e0b
Akshay Bhat <AKSHAYUBHAT@GMAIL.COM>
2010-05-26 14:09:20 -0400

    Improved the code using defaultdict(int) and by creating an AdjDict class with appropriate missing function to initialize the array now runs in 9 seconds

--- LP.py
+++ LP.py
@@ -9,45 +9,53 @@ WebSite:    http://www.akshaybhat.com
 
 
 """
-import random, time, sys, array
-from multiprocessing import Pool,Array,cpu_count
+import random, time, sys, array, logging, collections
+from multiprocessing import Pool, cpu_count
 
 # Global Variable THREAD defines number of processes to be used
 THREADS = cpu_count() 
 
 
+
+class AdjDict(dict):
+    """
+     A special Dictionary Class to hold adjecany list
+    """
+    def __missing__(self, key):
+        """
+        Missing is changed such that when a key is not found an integer array is initialized
+        """
+        self.__setitem__(key,array.array('i'))
+        return self[key]
+
+
 def maxVote(nLabels):
-    count = {}
-    maxList = []
-    maxCount = 0
-    for nLabel in nLabels:
-        if nLabel in count:
-            count[nLabel] += 1
-        else:
-            count[nLabel] = 1
-    #Check if the count is max
-        if count[nLabel] > maxCount:
-            maxCount = count[nLabel];
-            maxList = [nLabel,]
-        elif count[nLabel]==maxCount:
-            maxList.append(nLabel)
-    return random.choice(maxList)        
-                
+    cnt = collections.defaultdict(int)
+    if nLabels==[]:
+        return None
+    for i in nLabels:
+        cnt[i] += 1
+    maxv = max(cnt.itervalues())
+    return random.choice([k for k,v in cnt.iteritems() if v == maxv])
+
 
 
                 
 if __name__ == '__main__':
-    Adj = {}      # A Python Dictionary since it allows faster acess
+    Adj = AdjDict()     # A Python Dictionary since it allows faster acess
 
     #Parse the Command line Options
+    # filename
     if len(sys.argv)>1:
         filename = sys.argv[1] 
     else:
-        filename = 'Network.txt'
+        'Please specify file name E.g. LP.py Network.txt'
 
     if len(sys.argv)>2:
         iterations = int(sys.argv[2]) 
     else:
+        print " Number of Iterations not specified, using 5 iterations "
+        print " specify iterations use E.g. LP.py Network.txt 10"
         iterations = 5
 
         
@@ -64,20 +72,17 @@ if __name__ == '__main__':
         except:
             print " error while reading the file on line:", entry
         else:
-            Label[source] = source  #initialize the label to itself 
+            Label[source] = source  # initialize the label to itself 
             Label[target] = target  # just to be sure initialize the target as well
-            if not(source in Adj):
-                Adj[source] = [target,] #initialize the list
-            else:
-                Adj[source] += [target,]
-            if not(target in Adj):
-                Adj[target]=[]
+            Adj[source].append(target)
+
+            
     data.close()
 
-    # Enumerate all nodes 
+    #Enumerate all nodes 
     MapKeys = Adj.keys()
 
-
+    pool = Pool(processes=THREADS)
     for iteration in range(1,iterations+1):
         start=time.time()
 
@@ -86,12 +91,12 @@ if __name__ == '__main__':
         random.shuffle(MapKeys)
 
         for key in MapKeys:
-            temp=[]
+            temp=array.array('i')
             for target in Adj[key]:
                 temp.append(Label[target])
             MapInput.append(temp)
 
-        pool = Pool(processes=THREADS)
+        result=[]
         result = pool.map(maxVote,MapInput)
 
         index=0;
@@ -102,4 +107,4 @@ if __name__ == '__main__':
         print "Number of Communities:",len(set(result))," iteration:",iteration  
         print "Time Taken: ",time.time()-start
 
-    pool.close()
+    #pool.close()
